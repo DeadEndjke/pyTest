37e0c1f2f05fa7773d4902d982a18d137044d2ba
unknown <perceptionarmy@icloud.com>
2023-03-19 18:22:24 +0700

    finish

--- db.py
+++ db.py
@@ -1,72 +1,52 @@
 
 import sqlite3 as sq
-from user import User
 
-class UserDAO:
-    def __init__(self, dbname):
-        self.__dbname = dbname
-        with sq.connect(self.__dbname) as con:
-            cur = con.cursor()
-            
-            cur.execute("""create table if not exists user(
-                id integer primary key,
-                BASE_URL text not null default 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/',
-                API_KEY text not null,
-                LANGUAGE text not null default 'ru' check(LANGUAGE = 'ru' or LANGUAGE = 'en')
-            )""")
+class SettingsDAO:
+    def __init__(self, dbname:str):
+        self.__con = sq.connect(dbname)
+        self.__cur = self.__con.cursor()
 
-    def showAll(self):
-        with sq.connect(self.__dbname) as con:
-            cur = con.cursor()
-
-            res = cur.execute("select * from user")
-            return res.fetchall()
-
-    def get_max_id(self):
-         with sq.connect(self.__dbname) as con:
-            cur = con.cursor()
-
-            cur.execute("select max(id) from user")
-            result = cur.fetchone()
-            return result[0]
-
-
-            
-
-    def add_user(self, user):
-        if isinstance(user, User):
-            self.__user = user
-            with sq.connect(self.__dbname) as con:
-                cur = con.cursor()
-                cur.execute("insert into user (API_KEY, LANGUAGE) values('" + str(user.key)+"','" + str(user.lang)+"')")
-
-    def get_key(self, id):
-        with sq.connect(self.__dbname) as con:
-            cur = con.cursor()
-            cur.execute("select API_KEY from user where id=" + id)
-            result = cur.fetchone()
-            return result[0]
-    
-    def get_url(self, id):
-        with sq.connect(self.__dbname) as con:
-            cur = con.cursor()
-            cur.execute("select BASE_URL from user where id=" + id)
-            result = cur.fetchone()
-            return result[0]
+        self.__cur.execute("""create table if not exists settings(
+            id integer primary key,
+            BASE_URL text not null default 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/',
+            API_KEY text,
+            LANGUAGE text not null default 'ru' check(LANGUAGE = 'ru' or LANGUAGE = 'en')
+        )""")
+        if len(self.get_settings()) == 0:
+            with self.__con:
+                self.__cur.execute("insert into settings (API_KEY) values('')")
         
-    def get_lang(self, id):
-        with sq.connect(self.__dbname) as con:
-            cur = con.cursor()
-            cur.execute("select LANGUAGE from user where id=" + id)
-            result = cur.fetchone()
-            return result[0]
+    
+    def get_settings(self) -> list:
+        return self.__cur.execute("select * from settings").fetchall()
+
+    # def add_user(self, user:User):
+    #     if isinstance(user, User):
+    #         with self.__con:
+    #             self.__cur.execute("insert into user (API_KEY, LANGUAGE) values(?, ?)", (user.key, user.lang))
+
+    # def get_id(self, user:User) -> str:
+    #     if isinstance(user, User):
+    #         return self.__cur.execute("select id from user where API_KEY=? and LANGUAGE=?",(user.key, user.lang)).fetchone[0]
+
+    def get_key(self) -> str:
+        return self.__cur.execute("select API_KEY from settings where id=1").fetchone()[0]
+    
+    def get_url(self) -> str:
+        return self.__cur.execute("select BASE_URL from settings where id=1").fetchone()[0]
+        
+    def get_lang(self) -> str:
+        return self.__cur.execute("select LANGUAGE from settings where id=1").fetchone()[0]
+    
+    def set_key(self, key):
+        with self.__con:
+            self.__cur.execute("update settings set API_KEY=? where id=1", [key])
+
+    def set_lang(self, lang):
+        with self.__con:
+            self.__cur.execute("update settings set LANGUAGE=? where id=1", [lang])
             
-   
 
-
-if __name__ == "__main__":
-    user = UserDAO("db.db")
-    user.get_max_id()
         
 
     
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import json
 import requests
-from db import UserDAO
-from user import User
+from db import SettingsDAO
 
 
-def suggest(query, resource, userDAO, id):
-    url = userDAO.get_url(id) + resource
+def suggest(query, resource, settingsDAO) -> dict:
+    url = settingsDAO.get_url() + resource
     headers = {
-        'Authorization': 'Token ' + userDAO.get_key(id),
+        'Authorization': 'Token ' + settingsDAO.get_key(),
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     }
     data = {
         'query': query,
-        'language': userDAO.get_lang(id)
+        'language': settingsDAO.get_lang()
     }
     res = requests.post(url, data=json.dumps(data), headers=headers)
     return res.json()
 
+    
+def set_settings():
+    key:str=input("введите свой API_KEY: ")
+    lang:str = ''
+    while lang != "ru" and lang != "en":
+        lang=input("введите язык на котором будет работать программа 'ru' или 'en': ")
+    settingsDAO.set_key(key)
+    settingsDAO.set_lang(lang)
 
 
 
@@ -25,26 +32,29 @@ def suggest(query, resource, userDAO, id):
 
 if __name__ == "__main__":
     while True:
-        userDAO = UserDAO("db.db")
-        print("1.выбрать уже существующий аккаунт")
-        print("2.создать новый аккаунт")
-        print("3.выход")
-        ans = input("выберите действие 1, 2 или 3 и нажмите enter ")
+        settingsDAO = SettingsDAO("db.db")
+        print("1.использовать существующие настройки \n2.изменить настройки\n3.выход: ")
+        ans:str = input("выберите действие 1, 2 или 3 и нажмите enter: ")
+
         if ans == '1':
-            users = userDAO.showAll()
-            if(len(users)) == 0:
-                print("не созданно ни одного аккаунта")
+            settings:list = settingsDAO.get_settings()[0]
+            print("ваш API_KEY - ", settings[2])
+            print("ваш язык - ", settings[3])
+            
+            ans:str = input("оставить эти настройки? введите 'y' или 'n': ")
+
+            if ans == 'y':
+                pass
+
+            elif ans == 'n':
+                set_settings()
                 continue
-            else:
-                selected = input("выберите аккаунт или напишите 'exit' для выхода ")
-            if selected == 'exit':
-                break
+
+
         elif ans == '2':
-            key=input("введите свой API_KEY ")
-            lang=input("введите язык на котором будет работать программа 'ru' или 'en' ")
-            user = User(key, lang)
-            userDAO.add_user(user)
-            selected = userDAO.get_max_id()
+            set_settings()
+            continue
+
         elif ans == '3':
             break
 
@@ -54,19 +64,21 @@ if __name__ == "__main__":
             if(query == 'exit'):
                 break
 
-            data = suggest(query, 'address', userDAO, str(selected))
+            data:dict = suggest(query, 'address', settingsDAO)
 
-        
-            for i in range(0, len(data['suggestions'])):
-                print(str(i+1), data['suggestions'][i]['value'])
+            try:
+                for i in range(0, len(data['suggestions'])):
+                    print(str(i+1), data['suggestions'][i]['value'])
 
-            print()
+                coordinates:int = input("\nвыберите адрес для которого нужно напечатать координаты или 'exit' для выхода: ")
+                if(coordinates == 'exit'):
+                    break
 
-            coordinates:int = input("выберите адрес для которого нужно напечатать координаты или 'exit' для выхода: ")
-            if(coordinates == 'exit'):
+                print(data['suggestions'][int(coordinates)-1]['data']['geo_lat'], data['suggestions'][int(coordinates)-1]['data']['geo_lon'])
+
+            except Exception:
+                print("неверно введён API_KEY")
                 break
-
-            print(data['suggestions'][int(coordinates)-1]['data']['geo_lat'], data['suggestions'][int(coordinates)-1]['data']['geo_lon'])
 
         
 
